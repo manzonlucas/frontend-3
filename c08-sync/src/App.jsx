5a17dd4aeb8306dc69715e425d1cf48244d1905c
manzonlucas <manzonlucasm@gmail.com>
2022-08-17 20:05:54 -0300

    add completed c08

---
+++ c08-sync/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Button from './Button'
+import "./App.css";
+
+export default class App extends Component {
+  colors = ["RED", "BLUE", "YELLOW", "GREEN", "ORANGE", "MAGENTA", "BROWN", "LIME"]
+
+  /* ESPACIO DE TRABAJO
+  - Armar la estructura para una Class Component
+  - Hacer un state con el arreglo de los colores
+  - Vamos a usar una funcion shuffle mas abajo y vamos a enviarla por props... que
+    formas tenemos para no perder el contexto?
+  */
+
+  state = {
+    colors: this.colors
+  }
+
+  shuffle = () => {
+    this.setState(prevState => {
+
+      console.log(this.state)
+
+      return {
+        colors: (prevState.colors.sort(() => Math.random() - 0.5))
+      }
+    }
+    )
+  }
+
+  /* ESPACIO DE TRABAJO
+  Funcion Shuffle
+  Aqui deberan hacer una funcion que me retorne un nuevo arreglo con los
+  colores mezclados
+  */
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="panel">
+          {this.colors.map(color => <div key={color} className={color}>{color}</div>)}
+        </div>
+        <Button fn={this.shuffle} />
+      </div>
+
+    )
+  }
+}
